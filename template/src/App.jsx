3b82c10a4d06afd82576b8f5e11b462bf52c104c
Barkhasbadi Ankhbileg <barhas16@gmail.com>
2024-12-01 00:52:14 +0800

    1.5

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -3,30 +3,30 @@ import Header from "./Header"
 import Total from "./Total"
 
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of the component',
-      exercises: 14
-    }
-  ]
-
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of the component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={parts} />
-      <Total total = {totalExercises} />
+      <Content part={course.parts} />
+      <Total total = {course.parts} />
     </div>
   )
 }
--- template/src/Total.jsx
+++ template/src/Total.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 const Total = (props) => {
+    const totalExercises = props.total.reduce((sum, part) => sum + part.exercises, 0);
+    
     return (
         <p>
-            Number of exercies {props.total}
+            Number of exercies {totalExercises}
         </p>
     )
 }
