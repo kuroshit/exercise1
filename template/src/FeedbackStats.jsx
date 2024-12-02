2c0f3cc98644f76266fbb42ffbed9d139870acf9
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-02 13:07:28 +0800

    uni care part 5

--- template/src/FeedbackStats.jsx
+++ template/src/FeedbackStats.jsx
@@ -1,3 +1,5 @@
+import StatisticsLine from "./StatisticsLine";
+
 const FeedbackStats = ({ feedback }) => {
   const { good, neutral, bad } = feedback;
   const total = good + neutral + bad;
@@ -9,19 +11,21 @@ const FeedbackStats = ({ feedback }) => {
       {total > 0 ? (
         <div>
           <div className="feedback-numbers">
-            <div>Good {good}</div>
-            <div>Neutral {neutral}</div>
-            <div>Bad {bad}</div>
+            <StatisticsLine text={"Good"} value={good} />
+            <StatisticsLine text={"Neutral"} value={neutral} />
+            <StatisticsLine text={"Bad"} value={bad} />
           </div>
 
-          <div>
-            <div className="feedback-stats">Total: {total}</div>
-            <div>Average: {average.toFixed(2)}</div>
-            <div>Positive: {positive.toFixed(2)}%</div>
+          <div className="feedback-stats">
+            <StatisticsLine text={"Total:"} value={total} />
+            <StatisticsLine text={"Average:"} value={average.toFixed(2)} />
+            <StatisticsLine text={"Positive:"} value={`${positive.toFixed(2)}%`} />
           </div>
         </div>
       ) : (
-        <div>No feedback given</div>
+        <div>
+          <p>No feedback given</p>
+        </div>
       )}
     </>
   );
---
+++ template/src/StatisticsLine.jsx
@@ -0,0 +1,9 @@
+const StatisticsLine = ({text, value}) => {
+    return (
+        <div>
+            <div>{text} {value}</div>
+        </div>
+    )
+}
+
+export default StatisticsLine;
