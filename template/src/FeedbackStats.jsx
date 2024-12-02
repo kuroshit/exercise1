83d2e15f0f434c22c0a8f3b473ba399de035c91d
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-02 12:50:51 +0800

    uni cafe step 4

--- template/src/FeedbackStats.jsx
+++ template/src/FeedbackStats.jsx
@@ -6,17 +6,23 @@ const FeedbackStats = ({ feedback }) => {
 
   return (
     <>
-      <div className="feedback-numbers">
-        <div>Good {good}</div>
-        <div>Neutral {neutral}</div>
-        <div>Bad {bad}</div>
-      </div>
+      {total > 0 ? (
+        <div>
+          <div className="feedback-numbers">
+            <div>Good {good}</div>
+            <div>Neutral {neutral}</div>
+            <div>Bad {bad}</div>
+          </div>
 
-      <div>
-        <div className="feedback-stats">Total: {total}</div>
-        <div>Average: {average.toFixed(2)}</div>
-        <div>Positive: {positive.toFixed(2)}%</div>
-      </div>
+          <div>
+            <div className="feedback-stats">Total: {total}</div>
+            <div>Average: {average.toFixed(2)}</div>
+            <div>Positive: {positive.toFixed(2)}%</div>
+          </div>
+        </div>
+      ) : (
+        <div>No feedback given</div>
+      )}
     </>
   );
 };
