42dc76ab27bdbf8b15052e310e75f841f9d9bb5d
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-02 13:51:19 +0800

    uni care step 6

--- template/src/FeedbackStats.jsx
+++ template/src/FeedbackStats.jsx
@@ -10,6 +10,41 @@ const FeedbackStats = ({ feedback }) => {
     <>
       {total > 0 ? (
         <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Feedback</th>
+                <th>Count</th>
+              </tr>
+            </thead>
+            
+            <tbody>
+              <tr>
+                <td>Good</td>
+                <td>{good}</td>
+              </tr>
+              <tr>
+                <td>Neutral</td>
+                <td>{neutral}</td>
+              </tr>
+              <tr>
+                <td>Bad</td>
+                <td>{bad}</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>{total}</td>
+              </tr>
+              <tr>
+                <td>Average</td>
+                <td>{average.toFixed(2)}</td>
+              </tr>
+              <tr>
+                <td>Positive</td>
+                <td>{`${positive.toFixed(2)}%`}</td>
+              </tr>
+            </tbody>
+          </table>
           <div className="feedback-numbers">
             <StatisticsLine text={"Good"} value={good} />
             <StatisticsLine text={"Neutral"} value={neutral} />
